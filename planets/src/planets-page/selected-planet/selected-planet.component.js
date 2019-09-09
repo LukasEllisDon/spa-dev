c6f1889a0393cf1a973062772da223fed7ef41d4
Justin McMurdie <mcmurdiej@gmail.com>
2019-09-08 18:31:36 -0600

    adding population

--- planets/src/planets-page/selected-planet/selected-planet.component.js
+++ planets/src/planets-page/selected-planet/selected-planet.component.js
@@ -40,6 +40,10 @@ export default function SelectedPlanet (props) {
         <div className='attributeTitle'>Terrain</div>
         <div className='attribute'>{selectedPlanet.terrain}</div>
       </div>
+      <div className='planetAttribute'>
+        <div className='attributeTitle'>Population</div>
+        <div className='attribute'>{formatPopulation(selectedPlanet.population)}</div>
+      </div>
       <NotablePeople people={selectedPlanet.residents} />
     </div>
   )
@@ -56,4 +60,53 @@ function formatDiameter (value) {
       return `${value} Kilometers (${value * 0.621371} Miles)`
     }
   }
+}
+function formatPopulation (value) {
+  if(value === undefined) {
+    return ''
+  } else {
+    const parsedValue = parseInt(value)
+    if (isNaN(parsedValue)) {
+      return value
+    } else {
+      return abbrNum(value)
+    }
+  }
+}
+
+// copied from stack overflow
+export function abbrNum (number, decPlaces = 2) {
+  // 2 decimal places => 100, 3 => 1000, etc
+  decPlaces = Math.pow(10, decPlaces)
+
+  // Enumerate number abbreviations
+  const abbrev = [ "k", "m", "b", "t" ]
+  let newNumber = number
+
+  // Go through the array backwards, so we do the largest first
+  for (let i=abbrev.length-1; i>=0; i--) {
+
+    // Convert array index to "1000", "1000000", etc
+    const size = Math.pow(10,(i+1)*3)
+
+    // If the number is bigger or equal do the abbreviation
+    if(size <= newNumber) {
+      // Here, we multiply by decPlaces, round, and then divide by decPlaces.
+      // This gives us nice rounding to a particular decimal place.
+      newNumber = Math.round(number*decPlaces/size)/decPlaces
+
+      // Handle special case where we round up to the next abbreviation
+      if((newNumber == 1000) && (i < abbrev.length - 1)) {
+        newNumber = 1
+        i++
+      }
+
+      // Add the letter for the abbreviation
+      newNumber += abbrev[i]
+
+      // We are done... stop
+      break
+    }
+  }
+  return newNumber
 }
